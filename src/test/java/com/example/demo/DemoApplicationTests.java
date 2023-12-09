8ecdfe2e6e6b39a7f903039099aefb42738f3221
locoli <563235247@qq.com>
2023-12-09 23:06:11 +0800

    pom

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("123");
     }
 
 }
