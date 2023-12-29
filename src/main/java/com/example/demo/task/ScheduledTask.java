6ce3c88ab98cd1cb5e15219bfc924af85aaff125
liuxuchao <lxc2814407529@163.com>
2023-12-29 16:55:07 +0800

    添加生成基础类

--- src/main/java/com/example/demo/task/ScheduledTask.java
+++ src/main/java/com/example/demo/task/ScheduledTask.java
@@ -1,8 +1,12 @@
 package com.example.demo.task;
 
 import net.javacrumbs.shedlock.core.SchedulerLock;
+import okhttp3.*;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @descriptions:
@@ -21,6 +25,48 @@ public class ScheduledTask {
         Thread.sleep(12000);
         System.out.println("定时任务执行!!!!");
         System.out.println(System.currentTimeMillis());
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        try {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .connectTimeout(30, TimeUnit.SECONDS) // 增加连接超时时间为30秒
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .build();
+
+            MediaType mediaType = MediaType.parse("application/json");
+
+            // 替换为你的 API 密钥和模型 ID
+            String apiKey = "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";
+            String modelID = "gpt-3.5-turbo";
+
+            // 构建请求体
+            String requestBody = "{\"prompt\": \"你好，我是ChatGPT。\", \"max_tokens\": 50}";
+
+            RequestBody body = RequestBody.create(mediaType, requestBody);
+
+            // 构建请求
+            Request request = new Request.Builder()
+                    // .url("https://api.openai.com/v1/engines/" + modelID + "/completions")
+                    .url("https://api.openai.com/v1/engines")
+                    .post(body)
+                    .addHeader("Content-Type", "application/json")
+                    .addHeader("Authorization", "Bearer " + apiKey)
+                    .build();
+
+            // 发送请求并获取响应
+            Response response = client.newCall(request).execute();
+
+            if (response.isSuccessful() && response.body() != null) {
+                String responseBody = response.body().string();
+                System.out.println("Response: " + responseBody);
+            } else {
+                System.out.println("Request failed");
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
     }
 }
